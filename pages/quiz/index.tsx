5f5f604759939a42a4ab88f0b38bbf69647b270a
Brainvader <brainvade@gmail.com>
2021-10-03 17:55:50 +0900

    Add function to list file path

--- pages/quiz/index.tsx
+++ pages/quiz/index.tsx
@@ -1,5 +1,8 @@
+import path from 'path'
+
 import { GetStaticPropsContext, GetStaticPropsResult } from 'next'
 
+import { walk } from '../../utils/helper'
 
 interface QuizListProps {
     quizzes: string[]
@@ -17,15 +20,12 @@ export default function QuizList({ quizzes }: QuizListProps) {
 }
 
 export async function getStaticProps(context: GetStaticPropsContext): Promise<GetStaticPropsResult<QuizListProps>> {
+    const quizDir = path.join(process.cwd(), 'quiz')
+    const fileList: string[] = await walk(quizDir, [])
+
     return {
         props: {
-            quizzes: [
-                "React",
-                "Unity",
-                "C#",
-                "TypeScript",
-                "Algorithm"
-            ]
+            quizzes: fileList
         }
     }
 }
